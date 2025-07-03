a68885fa22b8dfd985c477b7474e06ce40ac5591
Grandejosh <grande_josh@odontologos.com>
2025-07-02 23:00:21 -0500

    c
    c

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -38,6 +38,7 @@
                             </p>
                             <div class="request-form">
                                 <form action="{{ route('apisubscriber') }}" method="post" id="ajax_contact" class="form-horizontal">
+                                    @csrf
                                     <div class="form-group row">
                                         <div class="col-md-6">
                                             <div class="form-item">
@@ -78,7 +79,7 @@
                                         </div>
                                     </div>
                                     <div class="submit-btn">
-                                        <button id="submit" class="ed-primary-btn" type="submit">Enviar Mensaje</button>
+                                        <button id="submitPageContactButton" class="ed-primary-btn" type="submit">Enviar Mensaje</button>
                                     </div>
                                 </form>
                                 <div id="form-messages" class="alert mt-20"></div>
@@ -129,6 +130,64 @@
                     </div>
                 </div>
             </div>
+            <script>
+        let form = document.getElementById('pageContactForm');
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+
+            var formulario = document.getElementById('pageContactForm');
+            var formData = new FormData(formulario);
+
+            // Deshabilitar el botón
+            var submitButton = document.getElementById('submitPageContactButton');
+            submitButton.disabled = true;
+            submitButton.style.opacity = 0.25;
+
+            // Crear una nueva solicitud XMLHttpRequest
+            var xhr = new XMLHttpRequest();
+
+            // Configurar la solicitud POST al servidor
+            xhr.open('POST', "{{ route('apisubscriber') }}", true);
+
+            // Configurar la función de callback para manejar la respuesta
+            xhr.onload = function() {
+                // Habilitar nuevamente el botón
+                submitButton.disabled = false;
+                submitButton.style.opacity = 1;
+                if (xhr.status === 200) {
+                    var response = JSON.parse(xhr.responseText);
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Enhorabuena',
+                        text: response.message,
+                        customClass: {
+                            container: 'sweet-modal-zindex' // Clase personalizada para controlar el z-index
+                        }
+                    });
+                    formulario.reset();
+                } else if (xhr.status === 422) {
+                    var errorResponse = JSON.parse(xhr.responseText);
+                    // Maneja los errores de validación aquí, por ejemplo, mostrando los mensajes de error en algún lugar de tu página.
+                    var errorMessages = errorResponse.errors;
+                    var errorMessageContainer = document.getElementById('messagePageContact');
+                    errorMessageContainer.innerHTML = 'Errores de validación:<br>';
+                    for (var field in errorMessages) {
+                        if (errorMessages.hasOwnProperty(field)) {
+                            errorMessageContainer.innerHTML += field + ': ' + errorMessages[field].join(', ') +
+                                '<br>';
+                        }
+                    }
+                } else {
+                    console.error('Error en la solicitud: ' + xhr.status);
+                }
+
+
+            };
+
+            // Enviar la solicitud al servidor
+            xhr.send(formData);
+        });
+    </script>
         </section>
         <!-- ./ contact-section -->
 
