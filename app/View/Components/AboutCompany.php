8f978800ad773efe0b245155157901f2620a4fa2
Yisus Web <connexion.jesus@gmail.com>
2025-07-09 17:57:03 -0500

    erer

--- app/View/Components/AboutCompany.php
+++ app/View/Components/AboutCompany.php
@@ -5,15 +5,47 @@ namespace App\View\Components;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
+use Modules\CMS\Entities\CmsSection;
 
 class AboutCompany extends Component
 {
     /**
      * Create a new component instance.
      */
+    protected $aboimagen;
+    protected $aboheader;
+    protected $abobody;
     public function __construct()
     {
-        //
+        $this->aboimagen = CmsSection::where('component_id', 'benefits_6')
+            ->join('cms_section_items', 'section_id', 'cms_sections.id')
+            ->join('cms_items', 'cms_section_items.item_id', 'cms_items.id')
+            ->select(
+                'cms_items.content',
+                'cms_section_items.position'
+            )
+            ->orderBy('cms_section_items.position')
+            ->get();
+            
+        $this->aboheader = CmsSection::where('component_id', 'benefits_6')
+            ->join('cms_section_items', 'section_id', 'cms_sections.id')
+            ->join('cms_items', 'cms_section_items.item_id', 'cms_items.id')
+            ->select(
+                'cms_items.content',
+                'cms_section_items.position'
+            )
+            ->orderBy('cms_section_items.position')
+            ->get();
+            
+        $this->abobody = CmsSection::where('component_id', 'benefits_6')
+            ->join('cms_section_items', 'section_id', 'cms_sections.id')
+            ->join('cms_items', 'cms_section_items.item_id', 'cms_items.id')
+            ->select(
+                'cms_items.content',
+                'cms_section_items.position'
+            )
+            ->orderBy('cms_section_items.position')
+            ->get();
     }
 
     /**
@@ -21,6 +53,10 @@ class AboutCompany extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.about-company');
+        return view('components.about-company', [
+            'aboimagen' => $this->aboimagen,
+            'aboheader' => $this->aboheader,
+            'abobody' => $this->abobody,
+        ]);
     }
 }
