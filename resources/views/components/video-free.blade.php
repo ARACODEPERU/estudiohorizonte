5d455413000de1a9a81210a69c564cf02051e430
Yisus Web <connexion.jesus@gmail.com>
2025-07-10 08:57:24 -0500

    eer

--- resources/views/components/video-free.blade.php
+++ resources/views/components/video-free.blade.php
@@ -19,7 +19,7 @@
                         <div class="shape-1"></div>
                     </div>
                     <div class="video-img">
-                        {{ $videofree[2]->content }}
+                        <iframe width="100%" height="100%" src="{{ $videofree[2]->content }}" title="{{ $videofree[1]->content }}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                         {{-- <img src="themes/webpage/assets/img/new-update/video-img.png" alt="video">
                         <div class="video-btn">
                             <a class="video-popup venobox" data-autoplay="true" data-vbtype="video" href="https://youtu.be/JwC-Qx1lJso?feature=shared">
