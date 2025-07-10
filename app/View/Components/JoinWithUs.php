7e5122b7fc9d8ecd0250a6d5514ed689822a3f6f
Yisus Web <connexion.jesus@gmail.com>
2025-07-10 11:16:35 -0500

    hyu

--- app/View/Components/JoinWithUs.php
+++ app/View/Components/JoinWithUs.php
@@ -5,15 +5,25 @@ namespace App\View\Components;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
+use Modules\CMS\Entities\CmsSection;
 
 class JoinWithUs extends Component
 {
     /**
      * Create a new component instance.
      */
+    protected $join;
     public function __construct()
     {
-        //
+        $this->join = CmsSection::where('component_id', 'join_with_us_12')
+            ->join('cms_section_items', 'section_id', 'cms_sections.id')
+            ->join('cms_items', 'cms_section_items.item_id', 'cms_items.id')
+            ->select(
+                'cms_items.content',
+                'cms_section_items.position'
+            )
+            ->orderBy('cms_section_items.position')
+            ->get();
     }
 
     /**
@@ -21,6 +31,8 @@ class JoinWithUs extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.join-with-us');
+        return view('components.join-with-us', [
+            'join' => $this->join
+        ]);
     }
 }
--- resources/views/components/join-with-us.blade.php
+++ resources/views/components/join-with-us.blade.php
@@ -5,7 +5,7 @@
                     <div class="shape"><img src="{{ asset('themes/webpage/assets/img/new-update/shapes/content-shape-1.png') }}" alt="shape"></div>
                     <div class="col-lg-5">
                         <div class="content-img-8">
-                            <img src="themes/webpage/assets/img/new-update/content-img-4.png" alt="content">
+                            <img src="{{ asset('storage/'.$join[0]->content) }}" alt="content">
                         </div>
                     </div>
                     <div class="col-lg-7">
@@ -16,15 +16,14 @@
                                         {{-- <i class="fa-sharp fa-solid fa-bolt"></i> --}}
                                         <img src="{{ asset('themes/webpage/assets/img/favicon.png') }}" alt="icon" style="width: 15px;">
                                     </span>
-                                    Únete a nosotros
+                                    {{ $join[1]->content }}
                                 </h4>
                                 <h2 class="section-title wow fade-in-bottom" data-wow-delay="400ms">
-                                    Conviértete en instructor y <br>Únete a nosotros
+                                    {{ $join[2]->content }}
                                 </h2>
                             </div>
                             <p class="mb-30 wow fade-in-bottom" data-wow-delay="500ms">
-                                En <b>Estudio Horizonte</b> buscamos profesionales apasionados por enseñar y compartir su experiencia. Al convertirte en instructor, 
-                                formarás parte de una comunidad comprometida con la educación de calidad, flexible y accesible.
+                                {{ $join[3]->content }}
                             </p>
                             <div class="content-btn wow fade-in-bottom" data-wow-delay="600ms">
                                 <a href="{{ route('web_contact_us') }}" class="ed-primary-btn">Contactanos</a>
