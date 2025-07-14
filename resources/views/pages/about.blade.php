0c7675f5eb4d242bdf640fa40d40388a4a340333
Yisus Web <connexion.jesus@gmail.com>
2025-07-14 08:58:37 -0500

    page header

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -7,21 +7,7 @@
         <x-header/>
         <!-- /.Main Header -->
 
-        <section class="page-header" style="padding: 60px;">
-            <div class="bg-img">
-                <img src="{{ asset('themes/webpage/assets/img/bg-img/page-header-bg.jpg') }}" alt="bg" style="width: 100%;">
-            </div>
-            <div class="container">
-                <div class="page-header-content">
-                    <h1 class="title">Sobre Nosotros</h1>
-                    <h4 class="sub-title">
-                        <a class="home" href="{{ route('index_main') }}">Home </a>
-                        <span class="icon">/</span>
-                        <a class="inner-page" href="{{ route('web_about') }}"> Nosotros</a>
-                    </h4>
-                </div>
-            </div>
-        </section>
+        <x-page-header/>
         <!-- ./ page-header -->
 
         <section class="about-section pt-120 pb-120">
