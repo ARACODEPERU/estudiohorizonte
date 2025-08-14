4bf21a4d188a75ebae80d6a9f39e241e9164b752
Yisus Web <connexion.jesus@gmail.com>
2025-08-13 23:35:06 -0500

    ghh

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,69 +3,69 @@
 @section('content')
 
 
-        <!-- header-area-start -->
-        <x-header/>
-        <!-- /.Main Header -->
+    <!-- header-area-start -->
+    <x-header />
+    <!-- /.Main Header -->
 
-        <div id="popup-search-box">
-            <div class="box-inner-wrap d-flex align-items-center">
-                <form id="form" action="#" method="get" role="search">
-                    <input id="popup-search" type="text" name="s" placeholder="Type keywords here...">
-                </form>
-                <div class="search-close"><i class="fa-sharp fa-regular fa-xmark"></i></div>
-            </div>
+    <div id="popup-search-box">
+        <div class="box-inner-wrap d-flex align-items-center">
+            <form id="form" action="#" method="get" role="search">
+                <input id="popup-search" type="text" name="s" placeholder="Type keywords here...">
+            </form>
+            <div class="search-close"><i class="fa-sharp fa-regular fa-xmark"></i></div>
         </div>
-        <!-- /#popup-search-box -->
+    </div>
+    <!-- /#popup-search-box -->
 
-        <div class="mobile-side-menu">
-            <div class="side-menu-content">
-                <div class="side-menu-head">
-                    <a href=""><img src="themes/webpage/assets/img/logo/logo-1.png" alt="logo"></a>
-                    <button class="mobile-side-menu-close"><i class="fa-regular fa-xmark"></i></button>
-                </div>
-                <div class="side-menu-wrap"></div>
-                <ul class="side-menu-list">
-                    <li><i class="fa-light fa-location-dot"></i>Address : <span>Amsterdam, 109-74</span></li>
-                    <li><i class="fa-light fa-phone"></i>Phone : <a href="tel:+01569896654">+01 569 896 654</a></li>
-                    <li><i class="fa-light fa-envelope"></i>Email : <a href="mailto:info@example.com">info@example.com</a></li>
-                </ul>
+    <div class="mobile-side-menu">
+        <div class="side-menu-content">
+            <div class="side-menu-head">
+                <a href=""><img src="themes/webpage/assets/img/logo/logo-1.png" alt="logo"></a>
+                <button class="mobile-side-menu-close"><i class="fa-regular fa-xmark"></i></button>
             </div>
+            <div class="side-menu-wrap"></div>
+            <ul class="side-menu-list">
+                <li><i class="fa-light fa-location-dot"></i>Address : <span>Amsterdam, 109-74</span></li>
+                <li><i class="fa-light fa-phone"></i>Phone : <a href="tel:+01569896654">+01 569 896 654</a></li>
+                <li><i class="fa-light fa-envelope"></i>Email : <a href="mailto:info@example.com">info@example.com</a></li>
+            </ul>
         </div>
-        <!-- /.mobile-side-menu -->
-        <div class="mobile-side-menu-overlay"></div>
+    </div>
+    <!-- /.mobile-side-menu -->
+    <div class="mobile-side-menu-overlay"></div>
 
-        {{-- <div id="preloader">
+    {{-- <div id="preloader">
             <div class="spinner-logo"><img src="themes/webpage/assets/img/favicon.png" alt="logo"></div>
             <div class="spinner"></div>
         </div> --}}
-        <!-- ./ preloader -->
+    <!-- ./ preloader -->
 
-        <x-slider />
-        <!-- ./ hero-section -->
+    <x-slider />
+    <!-- ./ hero-section -->
 
-        <x-category />
-        <!-- ./ category-section -->
+    <x-category />
+    <!-- ./ category-section -->
 
-        <x-service-promo />
-        <!-- ./ service-promo -->
+    <x-service-promo />
+    <!-- ./ service-promo -->
 
-        <x-about-company />
-        <!-- ./ about-section -->
+    <x-about-company />
+    <!-- ./ about-section -->
 
-        <x-courses-new />
-        <!-- ./ course-section -->
+    {{-- <x-courses-new /> --}}
+    <!-- ./ course-section -->
 
 
     <section class="feature-course pt-120 pb-120">
-            <div class="container">
-                <div class="feature-course-top heading-space">
-                    <div class="section-heading mb-0">
-                        <h4 class="sub-heading wow fade-in-bottom" data-wow-delay="200ms">
-                            <span class="heading-icon"><i class="fa-sharp fa-solid fa-bolt"></i></span>Nuevos Ingresos
-                        </h4>
-                        <h2 class="section-title wow fade-in-bottom" data-wow-delay="400ms">Explora nuestra formación</h2>
-                    </div>
-                    {{-- <ul class="course-nav nav nav-tabs mb-40" id="myTab-2" role="tablist">
+        <div class="container">
+            <div class="feature-course-top heading-space">
+                <div class="section-heading mb-0">
+                    <h4 class="sub-heading wow fade-in-bottom" data-wow-delay="200ms">
+                        <span class="heading-icon"><i class="fa-sharp fa-solid fa-bolt"></i></span>Nuevos Ingresos
+                    </h4>
+                    <h2 class="section-title wow fade-in-bottom" data-wow-delay="400ms">Explora nuestra formación</h2>
+                </div>
+                {{-- <ul class="course-nav nav nav-tabs mb-40" id="myTab-2" role="tablist">
                         <li class="nav-item" role="presentation">
                             <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab"
                                     aria-controls="home" aria-selected="true">All Categories
@@ -92,33 +92,39 @@
                             </button>
                         </li>
                     </ul> --}}
-                </div>
-                <div class="course-tab-content tab-content" id="myTabContent-2">
-                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                        <div class="row gy-4 justify-content-center">
-                            @foreach ($courses as $item)
+            </div>
+            <div class="course-tab-content tab-content" id="myTabContent-2">
+                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                    <div class="row gy-4 justify-content-center">
+                        @foreach ($courses as $item)
                             <div class="col-md-6">
                                 <div class="course-item">
                                     <div class="course-thumb-wrap">
                                         <div class="course-thumb">
-                                            <span class="offer" style=" z-index: 999; position: absolute; margin-top: 25px; background: #fff; ">Foro</span>
+                                            <span class="offer"
+                                                style=" z-index: 999; position: absolute; margin-top: 25px; background: #fff; ">Foro</span>
                                             <a href="{{ route('web_course_description', $item->id) }}">
-                                                <img src="{{ asset('storage/'.$item->course->image) }}" alt="course" style="width: 100%;">
+                                                <img src="{{ asset('storage/' . $item->course->image) }}" alt="course"
+                                                    style="width: 100%;">
                                             </a>
                                         </div>
                                     </div>
                                     <div class="course-content">
                                         <h3 class="title" style="font-size: 20px;">
-                                            <a href="{{ route('web_course_description', $item->id) }}">{{ $item->name }}</a>
+                                            <a
+                                                href="{{ route('web_course_description', $item->id) }}">{{ $item->name }}</a>
                                         </h3>
                                         <div class="course-author-box">
                                             <div class="course-author">
                                                 <div class="author-img">
-                                                    <img src="themes/webpage/assets/img/images/course-author-1.png" alt="course">
+                                                    <img src="{{ isset($item->course->teacher->person->image) ? asset('storage/'.$item->course->teacher->person->image) : '' }}"
+                                                        alt="course">
                                                 </div>
                                                 <div class="author-info">
                                                     <a href="">
-                                                        <h4 class="name">{{ $item->course->teacher->person->names . ' ' . $item->course->teacher->person->father_lastname }}</h4>
+                                                        <h4 class="name">
+                                                            {{ $item->course->teacher->person->names . ' ' . $item->course->teacher->person->father_lastname }}
+                                                        </h4>
                                                     </a>
                                                     <span>Instructor</span>
                                                 </div>
@@ -129,17 +135,18 @@
                                         </div>
                                     </div>
                                     <div class="bottom-content">
-                                        <a href="{{ route('web_course_description', $item->id) }}" class="course-btn">Leer Más</a>
+                                        <a href="{{ route('web_course_description', $item->id) }}" class="course-btn">Leer
+                                            Más</a>
                                         <a href="https://wa.link/g3i7et" class="ed-primary-btn-2">
                                             <i class="fab fa-whatsapp" aria-hidden="true"></i>&nbsp; Adquirir
                                         </a>
                                     </div>
                                 </div>
                             </div>
-                            @endforeach
-                        </div>
+                        @endforeach
                     </div>
-                    {{-- <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                </div>
+                {{-- <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
                         <div class="row gy-4 justify-content-center">
                             <div class="col-lg-4 col-md-6">
                                 <div class="course-item">
@@ -1139,31 +1146,31 @@
                             </div>
                         </div>
                     </div> --}}
-                </div>
             </div>
-             {{-- REPONER TERMINANDO EL PRIMER CURSO  --}}
+        </div>
+        {{-- REPONER TERMINANDO EL PRIMER CURSO  --}}
 
     </section>
 
-        <x-video-free />
-        <!-- ./ video-section -->
+    <x-video-free />
+    <!-- ./ video-section -->
 
-        {{-- <x-courses-recomendation /> --}}
-        <!-- ./ course-section -->
+    {{-- <x-courses-recomendation /> --}}
+    <!-- ./ course-section -->
 
-        <x-choose-us />
-        <!-- ./ choose-us -->
+    <x-choose-us />
+    <!-- ./ choose-us -->
 
-        <x-join-with-us />
-        <!-- ./ content-section -->
+    <x-join-with-us />
+    <!-- ./ content-section -->
 
-        <x-testimonies />
-        <!-- ./ testimonial-section -->
+    <x-testimonies />
+    <!-- ./ testimonial-section -->
 
-        <x-agreements />
-        <!-- ./ sponsor-section -->
+    <x-agreements />
+    <!-- ./ sponsor-section -->
 
-        {{-- <section class="blog-section pt-120 pb-120">
+    {{-- <section class="blog-section pt-120 pb-120">
             <div class="container">
                 <div class="section-heading text-center">
                     <h4 class="sub-heading wow fade-in-bottom" data-wow-delay="200ms"><span class="heading-icon"><i class="fa-sharp fa-solid fa-bolt"></i></span>News & Blogs</h4>
@@ -1224,9 +1231,9 @@
                 </div>
             </div>
         </section> --}}
-        <!-- ./ blog-section -->
+    <!-- ./ blog-section -->
 
-        <x-footer />
-        <!-- ./ footer-section -->
+    <x-footer />
+    <!-- ./ footer-section -->
 
 @stop
