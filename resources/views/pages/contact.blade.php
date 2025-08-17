138833d965347fc5dc3302bdda5fcae534c2d5cf
José Carlos <grande_josh@outlook.com>
2025-08-17 00:40:31 -0500

    d

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -101,33 +101,32 @@
                 // Configurar la solicitud POST al servidor
                 xhr.open('POST', "{{ route('apisubscriber') }}", true);
 
-                // Configurar la función de callback para manejar la respuesta
-                xhr.onload = function() {
-                    // Habilitar nuevamente el botón
-                    submitButton.disabled = false;
-                    submitButton.style.opacity = 1;
-                    if (xhr.status === 200) {
-                        var response = JSON.parse(xhr.responseText);
-                        Swal.fire({
-                            icon: 'success',
-                            title: 'Enhorabuena',
-                            text: response.message,
-                            customClass: {
-                                container: 'sweet-modal-zindex' // Clase personalizada para controlar el z-index
-                            }
-                        });
-                        formulario.reset();
-                    } else if (xhr.status === 422) {
-                        var errorResponse = JSON.parse(xhr.responseText);
-                        // Maneja los errores de validación aquí, por ejemplo, mostrando los mensajes de error en algún lugar de tu página.
-                        var errorMessages = errorResponse.errors;
-                        var errorMessageContainer = document.getElementById('messagePageContact');
-                        errorMessageContainer.innerHTML = 'Errores de validación:<br>';
-                        for (var field in errorMessages) {
-                            if (errorMessages.hasOwnProperty(field)) {
-                                errorMessageContainer.innerHTML += field + ': ' + errorMessages[field].join(', ') +
-                                    '<br>';
-                            }
+            // Configurar la función de callback para manejar la respuesta
+            xhr.onload = function() {
+                // Habilitar nuevamente el botón
+                submitButton.disabled = false;
+                submitButton.style.opacity = 1;
+                if (xhr.status === 200) {
+                    var response = JSON.parse(xhr.responseText);
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Enhorabuena',
+                        text: response.message,
+                        customClass: {
+                            container: 'sweet-modal-zindex' // Clase personalizada para controlar el z-index
+                        }
+                    });
+                    formulario.reset();
+                } else if (xhr.status === 422) {
+                    var errorResponse = JSON.parse(xhr.responseText);
+                    // Maneja los errores de validación aquí, por ejemplo, mostrando los mensajes de error en algún lugar de tu página.
+                    var errorMessages = errorResponse.errors;
+                    var errorMessageContainer = document.getElementById('messagePageContact');
+                    errorMessageContainer.innerHTML = 'Errores de validación:<br>';
+                    for (var field in errorMessages) {
+                        if (errorMessages.hasOwnProperty(field)) {
+                            errorMessageContainer.innerHTML += field + ': ' + errorMessages[field].join(', ') +
+                                '<br>';
                         }
                     } else {
                         console.error('Error en la solicitud: ' + xhr.status);
